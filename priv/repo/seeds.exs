7dd6383dabf807bf70036759d592b6c691b048da
Warren Jones <warren@pencethren.org>
2025-11-19 16:19:06 +0000

    feat: database seeds for pronouns

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,42 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+BathLARP.Repo.insert!(
+  %BathLARP.SiteConfiguration.Pronouns{
+    id: "bd932600-a091-42d6-a4d3-4399a1219b17",
+    subjective_personal: "he",
+    objective_personal: "him",
+    possessive: "his",
+    possessive_determiner: "his",
+    reflexive: "himself"
+  },
+  on_conflict: :replace_all,
+  conflict_target: :id
+)
+
+BathLARP.Repo.insert!(
+  %BathLARP.SiteConfiguration.Pronouns{
+    id: "f000fd12-c2fa-44cb-ad24-af8771e5b866",
+    subjective_personal: "she",
+    objective_personal: "her",
+    possessive: "her",
+    possessive_determiner: "hers",
+    reflexive: "herself"
+  },
+  on_conflict: :replace_all,
+  conflict_target: :id
+)
+
+BathLARP.Repo.insert!(
+  %BathLARP.SiteConfiguration.Pronouns{
+    id: "39057cf0-a98b-4dd4-a77c-8f0193783ad3",
+    subjective_personal: "they",
+    objective_personal: "them",
+    possessive: "their",
+    possessive_determiner: "theirs",
+    reflexive: "themself"
+  },
+  on_conflict: :replace_all,
+  conflict_target: :id
+)
